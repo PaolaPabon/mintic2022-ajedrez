7678dd1b3f0935c26c10c1953cf2282ed40f4776
PaolaPabon <jpaolapabonq@outlook.es>
2021-08-10 19:52:44 -0500

    CONSULTA EXITOSA

--- proyectoconstruccion01/src/main/java/co/edu/utp/misiontic2022/c2/reto3/App.java
+++ proyectoconstruccion01/src/main/java/co/edu/utp/misiontic2022/c2/reto3/App.java
@@ -2,15 +2,19 @@ package co.edu.utp.misiontic2022.c2.reto3;
 
 import java.sql.SQLException;
 
-import co.edu.utp.misiontic2022.c2.reto3.util.JDBCUtilities;
+import co.edu.utp.misiontic2022.c2.reto3.model.dao.ProyectosConsultaDao;
+import co.edu.utp.misiontic2022.c2.reto3.model.vo.ProyectosConsultaVo;
 
 public class App {
     public static void main(String[] args) {
 
         try {
-            var conn = JDBCUtilities.getConnection();
-            System.out.println("Motita feliz");
-            conn.close();
+            var dao = new ProyectosConsultaDao();
+            var lista = dao.listarProyectosPorClasificaciones("Casa Campestre", "Condominio");
+            for (ProyectosConsultaVo consulta: lista) {
+                System.out.println(consulta);
+            }
+                       
         } catch (SQLException e) {
             System.err.println("Error:" + e);
             e.printStackTrace();
---
+++ proyectoconstruccion01/src/main/java/co/edu/utp/misiontic2022/c2/reto3/model/dao/ComprasDeLiderDao.java
@@ -0,0 +1,5 @@
+package co.edu.utp.misiontic2022.c2.reto3.model.dao;
+
+public class ComprasDeLiderDao {
+    
+}
